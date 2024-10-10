bb028dfb9721930262ca34eff0f0fc737276ac57
maite <maite.sanchez@gm2dev.com>
2024-10-10 09:59:00 -0300

    feat(script.js): declare blockvar

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ const globaLVar = 1;
 
 function variables(){
     let functionVar = 30;
+    if(functionVar > globaLVar){
+        const blockVar = functionVar;
+    } else{
+        const blockVar = globaLVar;
+    }
 }
