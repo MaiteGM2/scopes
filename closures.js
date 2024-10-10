255b5411e15ecdd24f4829a25e86f50ddcf30b5f
maite <maite.sanchez@gm2dev.com>
2024-10-10 10:36:17 -0300

    feat(closures.js): add for loop in parameters function

--- closures.js
+++ closures.js
@@ -2,7 +2,9 @@ const globaLVar = 20;
 
 function parameters (paramVar){
     let functionVar = globaLVar-paramVar;
-    
+    for(let blockVar = 0; blockVar < functionVar; blockVar++){
+        nestedFunc(functionVar, blockVar);
+    }
 }
 
 window.onload = parameters(15);
